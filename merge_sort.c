dceeadea9e5661d03b5cb0a25ac7898a7ef22223
evgeniy <evgeniy.eg1@yandex.ru>
2017-10-26 15:40:50 +0300

    It was commited, because I'm going to try another variant of sorting chanks(now, no recusion)

--- merge_sort.c
+++ merge_sort.c
@@ -1,12 +1,14 @@
-/* C program for Merge Sort */
+/* C program for parallel Sort */
+//why concequence of headers is important
 #include <stdlib.h>
-#include <stdio.h>
-#include <malloc.h>
-#include <time.h>
 #include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int compare (const void * a, const void * b)
-{
+
+int compare (const void * a, const void * b) {
     return ( *(int*)a - *(int*)b );
 }
 
@@ -20,17 +22,15 @@ void printArray(int A[], int size)
 
 void mysort(int *arr, int l, int r){
     qsort(&arr[l], r - l, sizeof(int), compare);
-    printf("l=%d, r=%d", l , r);
-    printArray(arr, r);
 }
 
-void merge(int arr[], int l, int m, int r)
+void merge(int arr[], int l, int r)
 {
 	int i, j, k;
 	int n1 = m - l + 1;
 	int n2 = r - m;
     qsort(arr, r - l + 1 , sizeof(int), compare);
-    /* create temp arrays
+    // create temp arrays
 	//int L[n1], R[n2];
 	int *L = NULL;
 	int *R = NULL;
@@ -76,48 +76,84 @@ void merge(int arr[], int l, int m, int r)
 		k++;
 	}
 	free(L);
-	free(R);*/
+	free(R);
 }
 
+void merge(int *arr, int arr_size, int chank_size, int threads_amount, int chank_amount){
+    int l = 0;
+    int r = chank_size;
+    int m = r - (r - l) / 2;
+    for (i = 0; i < chank_amount; ++i) {
+        merge(arr, i * chank_size, (i + 1) * chank_size);
+        if (i + 1 == chank_amount){
+            cha
+        }
+    }
 
-void parallelSort(int *arr, int arr_size, int chank_size, int threads_amount) {
+}*/
+
+void parallelSort(int *arr, int arr_size, int chank_size, int threads_amount, int l, int r) {
+
+    //CHANK SORT
     int chank_amount = floor(arr_size / (double)chank_size);
-    printf("chank_am=%d\n", chank_amount);
     for (int i = 0; i < chank_amount; ++i){
-        printf("%d\n", i * chank_size);
+#pragma omp task firstprivate(i)
         mysort(arr, i * chank_size, (i + 1) * chank_size);
     }
-		//merge(arr, l, m, r);
+
+    //MERGING
+    //merge(arr, arr_size, chank_size, threads_amount, chank_amount);
+
+    if (r - l > chank_size) {
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        parallelSort(arr, l, m);
+        parallelSort(arr, m + 1, r);
+    }else{
+        merge(arr, l, m, r);
+    }
+
+
+
 }
 
 
 
 
 
-int main()
+int main(int argc, char* argv[])
 {
     int arr_size, chank_size, P;
-    scanf("%d", &arr_size);
-    scanf("%d", &chank_size);
-    scanf("%d", &P);
+    arr_size = atoi(argv[1]);
+    chank_size = atoi(argv[2]);
+    P = atoi(argv[3]);
+
 	int *arr = (int*)malloc(sizeof(int) * arr_size);
     srand(time(NULL));
+
     for (int i = 0; i < arr_size; ++i){
         arr[i] = rand() % 100;
     }
 
-	printf("Given array is \n");
-	printArray(arr, arr_size);
+
+    double whole_time = omp_get_wtime( );
+#pragma omp parallel
+    {
+#pragma omp single
+        parallelSort(arr, arr_size, chank_size, P);
+    }
+    whole_time = omp_get_wtime() - whole_time;
+    printf("parallel sort work: %f\n", whole_time);
 
 
+    for (int i = 0; i < arr_size; ++i){
+        arr[i] = rand() % 100;
+    }
+    whole_time = omp_get_wtime();
+    qsort(arr, arr_size, sizeof(int), compare);
+    whole_time = omp_get_wtime() - whole_time;
+    printf("library quicksort work: %f\n", whole_time);
 
-        //double whole_time = omp_get_wtime();
-    parallelSort(arr, arr_size, chank_size, P);
-    //qsort (arr, arr_size, sizeof(int), compare);
-
-    //whole_time = omp_get_wtime() - whole_time;
-    //printf("%d\n", whole_time);
-	printf("\nSorted array is \n");
-	printArray(arr, arr_size);
 	return 0;
 }

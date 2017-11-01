670601c48ee7200084287517fc7b690f50b1f686
Evgeniy <evgeniy.eg1@yandex.ru>
2017-11-01 19:17:46 +0300

    Done
    .

--- main.c
+++ main.c
@@ -46,31 +46,24 @@ void mysort(int *arr, int l, int r){
 void* merge_from_left(void *args){
 
     args_for_merge *chank = (args_for_merge*) args;
-    int amount_of_responsible_elements;
-    if ((chank->r - chank->l) % 2 == 1) {
-        amount_of_responsible_elements = (chank->r - chank->l) / 2+1;
-    }else {
-        amount_of_responsible_elements = (chank->r - chank->l) / 2;
-    }
 
     int i_l, j_l, k_l;
     i_l = 0; // Initial index of first subarray goes from left
     j_l = 0; // Initial index of second subarray goes from left
     k_l = chank->l; // Initial index of merged subarray goes from left
 
-    while (amount_of_responsible_elements > 0) {
-        --amount_of_responsible_elements;
+    while (42 == 42) {
         if (chank->L[i_l] < chank->R[j_l]) {
-            //printf("to %d (k_l)(I) insert %d\n", k_l, (chank->L[i_l]));
             *(chank->arr_begin + k_l) = chank->L[i_l];
             ++i_l;
             ++k_l;
         } else {
-            //printf("to %d (k_l)(J) insert %d i_l == %d n2 =%d\n", k_l, chank->R[j_l], j_l, chank->n2);
-            //printArray(chank->R, chank->n2);
             *(chank->arr_begin + k_l) = chank->R[j_l];
             ++j_l;
             ++k_l;
+        }
+        if (j_l >= chank->n2 || i_l >= chank->n1) {
+            break;
         }
     }
 }
@@ -79,39 +72,32 @@ void* merge_from_left(void *args){
 void* merge_from_right(void *args) {
     args_for_merge *chank = (args_for_merge *) args;
 
-    int amount_of_responsible_elements = (chank->r - chank->l) / 2 + 1;
-
-
     int i_r, j_r, k_r;
     i_r = chank->n1 - 1; // Initial index of first subarray goes from right
     j_r = chank->n2 - 1; // Initial index of second subarray goes from right
     k_r = chank->r; // Initial index of merged subarray goes from right
 
-    while (amount_of_responsible_elements > 0) {
-        --amount_of_responsible_elements;
+    while (42 == 42) {
         if (chank->L[i_r] >= chank->R[j_r]) {
             *(chank->arr_begin + k_r) = chank->L[i_r];
-            //printf("to %d (k_r)(I) insert %d\n", k_r, chank->L[i_r]);
-            //printArray(chank->L, chank->n2);
             --i_r;
             --k_r;
         } else {
-
-            //printf("to %d (k_r)(J) insert %d\n", k_r, chank->R[j_r]);
             *(chank->arr_begin + k_r) = chank->R[j_r];
             --j_r;
             --k_r;
+        }
+        if (j_r < 0 || i_r < 0) {
+            break;
         }
     }
 }
 
 void merge(int *arr, int left_bounder, int middle, int right_bounder, int max_thread)
 {
-    //printArray(&arr[left_bounder], right_bounder - left_bounder + 1);
     args_for_merge current_chank;
 
     current_chank.l = left_bounder;
-    //printf("lb%d rb%d", left_bounder, right_bounder);
     current_chank.r = right_bounder;
     current_chank.arr_begin = &arr[0];
     current_chank.n1 = middle - left_bounder + 1;
@@ -122,24 +108,20 @@ void merge(int *arr, int left_bounder, int middle, int right_bounder, int max_th
 
     memcpy(current_chank.L, &arr[left_bounder], current_chank.n1 * sizeof(int));
     memcpy(current_chank.R, &arr[middle +1], current_chank.n2 * sizeof(int));
+
     if (max_thread > 1) {
-        //printf("paral\n");
-        //pthread_barrier_init(&barrier, NULL, 2);
 
         pthread_t threads[2];
         pthread_create(&threads[0], NULL, merge_from_left, (void *) &current_chank);
         pthread_create(&threads[1], NULL, merge_from_right, (void *) &current_chank);
 
-        for (int i = 0; i < 2; ++i) {   //при маленьких секциях, потоки не успеват отработать
-                    //запускаются новые, и случается datarace. нужно ждать потоки.
+        for (int i = 0; i < 2; ++i) {
             pthread_join(threads[i], NULL);
         }
     }
     else{
         merge_from_left((void*) &current_chank);
         merge_from_right((void*) &current_chank);
-        //printArray(arr, right_bounder-left_bounder+1);
-        //printf("\n");
     }
     free(current_chank.L);
     free(current_chank.R);
@@ -170,7 +152,6 @@ void* initParallelSort(void* args){
         args[1].max_threads = arrArgs->max_threads;
 
         if (arrArgs->threadsExist < arrArgs->max_threads) {
-            //printf("paral:\n");
             pthread_t threads[2];
             for (int i = 0; i < 2; ++i) {
                 pthread_create(&threads[i], NULL, initParallelSort, (void*) &args[i]);
@@ -199,98 +180,69 @@ int main(int argc, char* argv[]) {
     int *arr2 = (int *) malloc(sizeof(int) * arr_size);
     srand(time(NULL));
 
-    int mythreads[5];
-    mythreads[0] = 1;
-    mythreads[1] = 2;
-    mythreads[2] = 4;
-    mythreads[3] = 8;
-    mythreads[4] = 16;
-    //for (int w = 0; w < 5; ++w) {
-      //  P = mythreads[w];
-      //  for (int q = 1000000; q < 10000000; q += 1000000) {
-        //    arr_size = q;
-          //  chank_size = q / 100;
-
 //create array
-            for (int i = 0; i < arr_size; ++i) {
-                arr[i] = rand() % 100;
-                //arr[i] = rand() % 10000;  //"%10000" is special for data.txt, for good view;
-                arr2[i] = arr[i];
-            }
+    for (int i = 0; i < arr_size; ++i) {
+        arr[i] = rand();
+        //arr[i] = rand() % 10000;  //"%10000" is special for data.txt, for good view;
+        arr2[i] = arr[i];
+    }
 //write array to data.txt
-            FILE *file;
-            file = fopen("data.txt", "a");
-            if (file == NULL) {
-                printf("Can't open data.txt!\n");
-                return 8;
-            }
+    FILE *file;
+    file = fopen("data.txt", "a");
+    if (file == NULL) {
+        printf("Can't open data.txt!\n");
+        return 8;
+    }
 
-            for (int i = 0; i < arr_size; ++i)
-                fprintf(file, "%d ", arr[i]);
-            fprintf(file, "\n");
+    for (int i = 0; i < arr_size; ++i)
+        fprintf(file, "%d ", arr[i]);
+    fprintf(file, "\n");
 
 //sorting, using OMP
-            double whole_time = omp_get_wtime();
+    double whole_time = omp_get_wtime();
+    paralSortArgs args;
+    args.arr = arr;
+    args.chank_sz = chank_size;
+    args.l = 0;
+    args.r = arr_size - 1;
+    args.depth = 0;
+    args.threadsExist = 1;
+    args.max_threads = P;
 
-            //printArray(arr, arr_size);
-            //printf("ready\n");
-            paralSortArgs args;
-            args.arr = arr;
-            args.chank_sz = chank_size;
-            args.l = 0;
-            args.r = arr_size - 1;
-            args.depth = 0;
-            args.threadsExist = 1;
-            args.max_threads = P;
+    initParallelSort((void *) &args);
 
-            arr[0] = 9;
-            arr[1] = 8;
-            arr[2] = 7;
-            arr[3] = 6;
-            arr[4] = 5;
-            arr[5] = 4;
-            arr[6] = 3;
-            arr[7] = 2;
-            arr[8] = 1;
-            arr[9] = -1;
-            memcpy(arr2, arr, 10*sizeof(int));
-
-            //printArray(arr, arr_size);
-            initParallelSort((void *) &args);
-            //printArray(arr, arr_size);
-
-            double whole_time_of_parallelSort = omp_get_wtime() - whole_time;
-            printf("parallel sort work: %f\n", whole_time_of_parallelSort);
+    double whole_time_of_parallelSort = omp_get_wtime() - whole_time;
+    printf("parallel sort work: %f\n", whole_time_of_parallelSort);
 
 //sorting, using library qsort
-            whole_time = omp_get_wtime();
-            qsort(arr2, arr_size, sizeof(int), compare);
-            whole_time = omp_get_wtime() - whole_time;
-            printf("library quicksort work: %f\n", whole_time);
+    whole_time = omp_get_wtime();
+    qsort(arr2, arr_size, sizeof(int), compare);
+    whole_time = omp_get_wtime() - whole_time;
+    printf("library quicksort work: %f\n", whole_time);
 
 //checking errors
-            for (int i = 0; i < arr_size; ++i)
-                if (arr[i] != arr2[i])
-                    printf("Всё пропало!!!\n");
+    for (int i = 0; i < arr_size; ++i)
+        if (arr[i] != arr2[i])
+            printf("Всё пропало!!!\n");
 
 //writing sorted array to data.txt
-            for (int i = 0; i < arr_size; ++i)
-                fprintf(file, "%d ", arr[i]);
-            fprintf(file, "\n\n");
-            fclose(file);
+    for (int i = 0; i < arr_size; ++i)
+        fprintf(file, "%d ", arr[i]);
+    fprintf(file, "\n\n");
+    fclose(file);
 
 //writing statistic
-            FILE *file2;
-            file2 = fopen("stats.txt", "a");
-            if (file2 == NULL) {
-                printf("Can't open file!\n");
-                return 8;
-            }
-            fprintf(file2, "%f %f %d %d %d \n", whole_time_of_parallelSort, whole_time, \
+    FILE *file2;
+    file2 = fopen("stats.txt", "a");
+    if (file2 == NULL) {
+        printf("Can't open file!\n");
+        return 8;
+    }
+    fprintf(file2, "%f %f %d %d %d \n", whole_time_of_parallelSort, whole_time, \
         arr_size, chank_size, P);
-            fclose(file2);
+    fclose(file2);
 
-     //   }
+    //   }
     //}
     free(arr);
     free(arr2);
